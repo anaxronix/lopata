c47069966aa308ee89a9b6f6f47475ea9a2c2f26
Anton Balashov <anton.i.balashov@gmail.com>
2017-05-05 18:42:33 +0300

    Add nex

--- js/app.js
+++ js/app.js
@@ -4,6 +4,133 @@
 console.log(React);
 console.log(ReactDOM);
 
+class CityForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event){
+        alert('A city was submitted: ' + this.state.value);
+          event.preventDefault();
+         }
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    City:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+
+var WeatherContainer = React.createClass({
+
+    getInitialState: function(){
+        return {
+            coord: [],
+            weather: [],
+            base: [],
+            main: [],
+            visibility: [],
+            wind: [],
+            clouds: [],
+            dt: [],
+            sys: [],
+            id: [],
+            name: [],
+            cod: []
+        }
+    },
+
+    componentDidMount: function(){
+        var _this = this,
+            city = 'Moscow';
+
+
+
+        axios.get('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=6421848aff2d6167b679e51b9ad01f8e').then(function(response){
+            _this.setState({
+                            coord: response.data.coord,
+                            weather: response.data.weather,
+                            base: response.data.base,
+                            main: response.data.main,
+                            visibility: response.data.visibility,
+                            wind: response.data.wind,
+                            clouds: response.data.clouds,
+                            dt: response.data.dt,
+                            sys: response.data.sys,
+                            id: response.data.id,
+                            name: response.data.name,
+                            cod: response.data.cod
+            })
+            console.log(response.data);
+            console.log(response.status);
+        });
+    },
+
+    render: function(){
+        return (
+            <div>
+                <p>  </p>
+                <p>City geo location, longitude: {this.state.coord.lon}</p>
+                <p>City geo location, latitude: {this.state.coord.lat}</p>
+
+                <p>Weather condition id: {this.state.weather.id}</p>
+                <p>Group of weather parameters (Rain, Snow, etc.): {this.state.weather.main}</p>
+                <p>Weather condition within the group: {this.state.weather.description}</p>
+                <p>Weather icon id: {this.state.weather.icon}</p>
+
+                <p>Internal parameter: {this.state.base}</p>
+
+                <p>Temperature. Unit Default Kelvin, Metric Celsius, Imperial Fahrenheit: {this.state.main.temp}</p>
+                <p>Atmospheric pressure (on the sea level): {this.state.main.pressure}</p>
+                <p>Humidity, %: {this.state.main.humidity}</p>
+                <p>Minimum temperature at the moment: {this.state.main.temp_min}</p>
+                <p>Maximum temperature at the moment: {this.state.main.temp_max}</p>
+
+                <p>???: {this.state.visibility}</p>
+
+                <p>Wind Speed: {this.state.wind.speed}</p>
+                <p>Wind direction, degress: {this.state.wind.deg}</p>
+
+                <p>Cloudiness, % : {this.state.clouds.all}</p>
+                <p>Time of data calculation, unix, UTC: {this.state.dt}</p>
+
+                <p>Internal parameter: {this.state.sys.type}</p>
+                <p>Internal parameter: {this.state.sys.id}</p>
+                <p>Internal parameter: {this.state.sys.message}</p>
+                <p>Country code (RU, DE, etc.): {this.state.sys.country}</p>
+                <p>Sunrise time, unix, UTC: {this.state.sys.sunrise}</p>
+                <p>Sunset time, unix, UTC: {this.state.sys.sunset}</p>
+
+                <p>City ID: {this.state.id}</p>
+                <p>City name: {this.state.name}</p>
+                <p>Internal parameter: {this.state.cod}</p>
+
+            </div>
+        );
+    }
+
+});
+
+
+
+/**
+
+
 var my_news = [
     {
         author: 'Sasha',
@@ -21,6 +148,8 @@ var my_news = [
         bigText: 'Never Never Never Never Never Never Never Never Never Never Never Never'
     }
 ];
+
+
 
 
 
@@ -80,21 +209,22 @@ var News = React.createClass({
         console.log(newsTemplate);
         return(
         <div className="news">
-            {newsTemplate}   {/* Not news, now. */}
+            {newsTemplate}
             <strong className= {'news_count' + (data.length > 0 ?'':'none')}>Vsego novostey: {data.length}</strong>
         </div>
         );
     }
 });
 
-
+**/
 
 var App = React.createClass({
     render: function(){
         return(
             <div className="app">
-                <h3> News</h3>
-                <News data={my_news} />
+                <CityForm />
+                <WeatherContainer />
+
             </div>
 
         );
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+
+
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(__dirname));
